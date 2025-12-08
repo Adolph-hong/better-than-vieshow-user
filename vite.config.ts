4a0224abebf02a97fbe459b0cc61e52db8b75fdb
iamlouxu <wayne199939@gmail.com>
2025-12-08 20:22:31 +0800

    chore: configure Vite server settings to allow host and specify allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
