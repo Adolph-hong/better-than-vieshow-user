c71de7acedba8e8d867b63ca053a45c612cc5101
Adolph-hong <zxcv010806056@gmail.com>
2025-12-18 17:11:52 +0800

    update: footer style

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,9 +1,11 @@
 import { useLocation, useNavigate } from "react-router-dom"
-import { Home, Search, Ticket, Settings, type LucideIcon } from "lucide-react"
+import { Search } from "lucide-react"
+import { House, Ticket, GearSix } from "phosphor-react"
 import light from "@/assets/icon/light.svg"
+import ticket from "@/assets/icon/ticket.svg"
 
 interface NavItem {
-  icon: LucideIcon
+  icon: React.ElementType
   path: string
 }
 interface bottomStyleProps {
@@ -11,10 +13,10 @@ interface bottomStyleProps {
 }
 
 const navItems: NavItem[] = [
-  { icon: Home, path: "/" },
+  { icon: House, path: "/" },
   { icon: Search, path: "/movie-search" },
   { icon: Ticket, path: "/ticket" },
-  { icon: Settings, path: "/settings" },
+  { icon: GearSix, path: "/settings" },
 ]
 
 const Footer = ({ bottomStyle = "bottom-0"}: bottomStyleProps) => {
@@ -22,7 +24,7 @@ const Footer = ({ bottomStyle = "bottom-0"}: bottomStyleProps) => {
   const navigate = useNavigate()
 
   return (
-    <footer className={`fixed ${bottomStyle} z-100 flex px-6 py-2 gap-3 bg-[#11968D] rounded-3xl`}>
+    <footer className={`fixed ${bottomStyle} z-100 flex px-6 py-2 gap-6 bg-[#11968D] rounded-3xl`}>
       {navItems.map(({ icon: Icon, path }) => {
         const isActive = location.pathname === path
         return (
@@ -33,11 +35,25 @@ const Footer = ({ bottomStyle = "bottom-0"}: bottomStyleProps) => {
             onClick={() => navigate(path)}
             aria-label={`Navigate to ${path}`}
           >
-            <Icon className={`z-10 ${isActive ? "text-white" : "text-[#2B514E]"}`} />
+            {path === "/ticket" ? (
+              <img
+                src={ticket}
+                alt="ticket"
+                className={`z-10 w-6 h-6 ${
+                  isActive ? "filter brightness-0 invert" : ""
+                }`}
+              />
+            ) : (
+              <Icon
+                size={24}
+                weight="fill"
+                className={`z-10 ${isActive ? "text-white" : "text-[#144D4A]"}`}
+              />
+            )}
             {isActive && (
               <div className="absolute -top-2 left-1/2 -translate-x-1/2 flex w-11 h-[41px] flex-col items-center">
                 <div className="w-10 h-1.25 bg-white rounded-sm" />
-                <img src={light} alt="light" />
+                <img src={light} alt="light" className="-mt-0.5" />
               </div>
             )}
           </button>
