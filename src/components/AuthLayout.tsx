71c6b4c82913ea08e53a3b9707991528f718a57d
iamlouxu <wayne199939@gmail.com>
2025-12-19 11:53:49 +0800

    style: update layout and spacing in AuthLayout component

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -21,14 +21,14 @@ const AuthLayout = ({ title, children, footer, paddingTop = "pt-[190px]" }: Auth
         </h1>
       </header>
 
-      <section className="px-4">
+      <section className="flex-1 px-4">
         <form className="mt-4 flex flex-col rounded-lg bg-[#1F1F1F] px-4 pb-4">
           <h2 className="pt-4 text-center text-xl font-semibold text-[#E5E5E5]">{title}</h2>
           {children}
         </form>
       </section>
 
-      <footer className="mt-auto pb-4 text-center">{footer}</footer>
+      <footer className="pb-4 text-center">{footer}</footer>
     </main>
   )
 }
