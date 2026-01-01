e3046108c268f5bcc667dfd9d008a9f795ad5eca
Adolph-hong <zxcv010806056@gmail.com>
2026-01-01 15:05:00 +0800

    update: sendapi

--- src/utils/sendAPI.ts
+++ src/utils/sendAPI.ts
@@ -1,12 +1,7 @@
 import { API_BASE_URL } from "@/config/api"
 
 const sendAPI = async (url: string, method: string = "GET", body: unknown = undefined) => {
-  if (!API_BASE_URL) {
-    throw new Error("VITE_API_BASE_URL 環境變數未設置")
-  }
-
-  const fullUrl = `${API_BASE_URL}${url}`
-  const response = await fetch(fullUrl, {
+  const response = await fetch(`${API_BASE_URL}${url}`, {
     method,
     headers: {
       "Content-Type": "application/json",
